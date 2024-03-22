f8e4177be429a7349431fe4f3d4acc135506d563
Rustem Hi <rustemxrid@icloud.com>
2024-03-22 10:08:01 +0300

    изменил высоту окна для мобильных телефонов

--- src/components/UserInterface/components/RightSide/UserInterfaceChatBar.jsx
+++ src/components/UserInterface/components/RightSide/UserInterfaceChatBar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+
+import { IoChevronBack } from 'react-icons/io5'
+
 import { Avatar } from '../../../components'
+
 
 const UserInterfaceChatBar = ({ currentChat, clearChat }) => {
   const { name, avatar } = currentChat
   
   return (
     <div className='user-interface-chat__bar'>
-      <button className='button user-interface-chat__bar-button' onClick={clearChat}>Back</button>
+      <button className='button user-interface-chat__bar-button' onClick={clearChat}><IoChevronBack size={32} /></button>
 
       <p className='user-interface-chat__bar-name'>{name}</p>
 
-      <div className='user-interface__chat__bar__image-container'>
+      <button className='button user-interface__chat__bar__image-button'>
         <Avatar imageClassName='user-interface-chat__bar-avatar' url={avatar} />
-      </div>
+      </button>
     </div>
   )
 }
